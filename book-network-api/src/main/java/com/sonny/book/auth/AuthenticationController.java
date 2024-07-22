1fc96ea80eb8881ffe76221c02b499bf0d2bca42
SonnyHardy <htambeck@gmail.com>
2024-07-22 17:35:05 +0200

    Account activation, login and JWT generation

--- book-network-api/src/main/java/com/sonny/book/auth/AuthenticationController.java
+++ book-network-api/src/main/java/com/sonny/book/auth/AuthenticationController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "auth")
 @RequiredArgsConstructor
 @Tag(name = "Authentication")
+@Slf4j
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
@@ -20,7 +22,20 @@ public class AuthenticationController {
     @ResponseStatus(HttpStatus.ACCEPTED)
     public ResponseEntity<?> register(@RequestBody @Valid RegistrationRequest request) throws MessagingException {
         this.authenticationService.register(request);
+        log.info("Inscription");
         return ResponseEntity.accepted().build();
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody @Valid AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(this.authenticationService.authenticate(request));
+    }
+
+    @GetMapping("/activate-account")
+    public void confirm(@RequestParam String token) throws MessagingException {
+        this.authenticationService.activateAccount(token);
     }
 
 }
