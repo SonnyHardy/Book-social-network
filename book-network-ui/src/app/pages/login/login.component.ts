fe0dadd6a75e4a69b5cef76ebb5f062801a82a5c
SonnyHardy <htambeck@gmail.com>
2024-09-17 15:56:56 +0200

    Implement the activate account page

--- book-network-ui/src/app/pages/login/login.component.ts
+++ book-network-ui/src/app/pages/login/login.component.ts
@@ -5,6 +5,7 @@ import {FormsModule} from "@angular/forms";
 import {Router} from "@angular/router";
 import {AuthenticationService} from "../../services/services/authentication.service";
 import {AuthenticationResponse} from "../../services/models/authentication-response";
+import {TokenService} from "../../services/token/token.service";
 
 @Component({
   selector: 'app-login',
@@ -27,7 +28,8 @@ export class LoginComponent {
 
   constructor(
     private router: Router,
-    private authService: AuthenticationService
+    private authService: AuthenticationService,
+    private tokenService: TokenService
   ) {
   }
 
@@ -38,7 +40,8 @@ export class LoginComponent {
       body: this.authRequest
     }).subscribe({
       next: (res: AuthenticationResponse): void => {
-        // save the token
+        // save the token and navigate in path 'books'
+        this.tokenService.token = res.token as string;  // we cast to a string because the token can be undefined
         this.router.navigate(['books']);
       },
       error: (err): void  => {
