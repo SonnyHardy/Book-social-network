e8d165dffc6cab0bf120703ebaba1aa86b3ccf58
SonnyHardy <htambeck@gmail.com>
2024-09-21 00:13:39 +0200

    Implement the My book page

--- book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
+++ book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -2,15 +2,17 @@ import {Component, OnInit} from '@angular/core';
 import {BookService} from "../../../../services/services/book.service";
 import {Router} from "@angular/router";
 import {PageResponseBookResponse} from "../../../../services/models/page-response-book-response";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {BookCardComponent} from "../../components/book-card/book-card.component";
+import {BookResponse} from "../../../../services/models/book-response";
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
   imports: [
     NgForOf,
-    BookCardComponent
+    BookCardComponent,
+    NgIf
   ],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss'
@@ -20,7 +22,9 @@ export class BookListComponent implements OnInit {
   bookResponse: PageResponseBookResponse = {};
 
   page: number = 0;
-  size: number = 5;
+  size: number = 4;
+  message: string = '';
+  level: string = 'success';
 
   constructor(
     private bookService: BookService,
@@ -43,27 +47,48 @@ export class BookListComponent implements OnInit {
   }
 
   goToFirstPage() {
-
+    this.page = 0;
+    this.findAllBooks();
   }
 
   goToPreviousPage() {
-
+    this.page --;
+    this.findAllBooks();
   }
 
-  goToPage(index: number) {
-
+  goToPage(pageIndex: number) {
+    this.page = pageIndex;
+    this.findAllBooks();
   }
 
   goToNextPage() {
-
+    this.page ++;
+    this.findAllBooks();
   }
 
   goToLastPage() {
-
+    this.page = this.bookResponse.totalPages as number - 1;
+    this.findAllBooks();
   }
 
   get isLastPage(): boolean {
     return this.page == this.bookResponse.totalPages as number - 1;
   }
 
+  borrowBook(book: BookResponse) {
+    this.message = '';
+    this.bookService.borrowBook({
+      'book-id': book.id as number
+    }).subscribe({
+      next: () => {
+        this.level = 'success';
+        this.message = 'Book successfully added to your list';
+      },
+      error: (err) => {
+        console.log(err);
+        this.level = 'error';
+        this.message = err.error.error;
+      }
+    });
+  }
 }
