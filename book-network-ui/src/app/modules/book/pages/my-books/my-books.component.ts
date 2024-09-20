c88aac88dde64757b30b15919c3dac14e0c93f58
SonnyHardy <htambeck@gmail.com>
2024-09-21 00:37:57 +0200

    Implement the create book method

--- book-network-ui/src/app/modules/book/pages/my-books/my-books.component.ts
+++ book-network-ui/src/app/modules/book/pages/my-books/my-books.component.ts
@@ -1,12 +1,88 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BookCardComponent} from "../../components/book-card/book-card.component";
+import {NgForOf, NgIf} from "@angular/common";
+import {PageResponseBookResponse} from "../../../../services/models/page-response-book-response";
+import {BookService} from "../../../../services/services/book.service";
+import {Router, RouterLink} from "@angular/router";
+import {BookResponse} from "../../../../services/models/book-response";
 
 @Component({
   selector: 'app-my-books',
   standalone: true,
-  imports: [],
+  imports: [
+    BookCardComponent,
+    NgForOf,
+    NgIf,
+    RouterLink
+  ],
   templateUrl: './my-books.component.html',
   styleUrl: './my-books.component.scss'
 })
-export class MyBooksComponent {
+export class MyBooksComponent implements OnInit{
 
+  bookResponse: PageResponseBookResponse = {};
+
+  page: number = 0;
+  size: number = 4;
+
+  constructor(
+    private bookService: BookService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.findAllBooksByOwner();
+  }
+
+  private findAllBooksByOwner() {
+    this.bookService.findAllBooksByOwner({
+      page: this.page,
+      size: this.size
+    }).subscribe({
+      next: (books) => {
+        this.bookResponse = books;
+      }
+    });
+  }
+
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllBooksByOwner();
+  }
+
+  goToPreviousPage() {
+    this.page --;
+    this.findAllBooksByOwner();
+  }
+
+  goToPage(pageIndex: number) {
+    this.page = pageIndex;
+    this.findAllBooksByOwner();
+  }
+
+  goToNextPage() {
+    this.page ++;
+    this.findAllBooksByOwner();
+  }
+
+  goToLastPage() {
+    this.page = this.bookResponse.totalPages as number - 1;
+    this.findAllBooksByOwner();
+  }
+
+  get isLastPage(): boolean {
+    return this.page == this.bookResponse.totalPages as number - 1;
+  }
+
+  archiveBook(book: BookResponse) {
+
+  }
+
+  shareBook(book: BookResponse) {
+
+  }
+
+  editBook(book: BookResponse) {
+
+  }
 }
