aeb212acae7e72d22458726115ccb2abefd916b2
SonnyHardy <htambeck@gmail.com>
2024-09-27 19:39:17 +0200

    Implement the backend pipeline

--- book-network-api/src/main/java/com/sonny/book/config/BeansConfig.java
+++ book-network-api/src/main/java/com/sonny/book/config/BeansConfig.java
@@ -1,6 +1,7 @@
 package com.sonny.book.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -27,6 +28,8 @@ import java.util.List;
 public class BeansConfig {
 
     private final UserDetailsService userDetailsService;
+    @Value("${application.cors.origins}")
+    private List<String> allowedOrigins;
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
@@ -55,8 +58,8 @@ public class BeansConfig {
     public CorsFilter corsFilter() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:8083"));
+        //config.setAllowCredentials(true);
+        config.setAllowedOrigins(allowedOrigins);
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.ORIGIN,
                 HttpHeaders.CONTENT_TYPE,
