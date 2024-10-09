af96c4706f34b733b7d52f012fb48b32e49a695f
SonnyHardy <htambeck@gmail.com>
2024-10-09 19:49:30 +0200

    Configure and implement the websocket with Spring Boot

--- book-network-api/src/main/java/com/sonny/book/security/SecurityConfig.java
+++ book-network-api/src/main/java/com/sonny/book/security/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
                                 "/configuration/security",
                                 "/swagger-ui/**",
                                 "/webjars/**",
-                                "/swagger-ui.html"
+                                "/swagger-ui.html",
+                                "/websocket/**"
                         ).permitAll()
                                 .anyRequest()
                                 .authenticated()
